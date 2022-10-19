a15791f31fce556118aef1110af24c1fd180afec
Rocinante <kazungu61@outlook.com>
2022-10-19 08:47:32 +0700

    Variable declaration

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int quant;
+    int choice;
+     //Quantiy
+     int Qroom{0}, Qpasta{0}, Qnoodles{0},Qburger{0},Qshake{0}, Qchicken{0};
+
+     //Items sold
+     int Srooms{0}, Spasta{0}, Snoodles{0}, Sburger{0},Sshake{0}, Schicken{0};
+
+     //Total price of items
+     int Total_rooms{0}, Total_pasta{0}, Total_noodles{0}, Total_burgers{0}, Total_shake{0}, Total_chicken{0};
+    return 0;
+}
